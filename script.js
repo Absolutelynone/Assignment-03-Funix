e02efe26696b6dd7ab08ccc83319afe8a30b8f4a
Absolutelynone <139056405+Absolutelynone@users.noreply.github.com>
2023-07-29 21:54:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+function handleSubmit() {
+  const emailValue = document.getElementById("email").value.toLocaleLowerCase();
+  const errorEmail = document.getElementById("error-email");
+  const checkMail =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  const check = emailValue.match(checkMail);
+  const sectionContent = document.querySelector(".personal-container");
+  const submitControl = document.querySelector(".submit-email");
+  if (check) {
+    sectionContent.style.display = "block";
+    submitControl.style.display = "none";
+    errorEmail.innerHTML = "";
+  } else {
+    errorEmail.innerHTML = "Vui lòng nhập đúng định dạng email";
+  }
+}
+
+function handleOnMouseOver(element) {
+  const viewMore = element.querySelector(".control-view");
+  viewMore.style.display = "inline-block";
+}
+
+function handleOnMouseOut(element) {
+  const viewMore = element.querySelector(".control-view");
+  if (!viewMore.classList.value.includes("view-less")) {
+    viewMore.style.display = "none";
+  }
+}
+
+function handleViewMore(element) {
+  const parentElement = element.closest(".parent");
+  const viewMore = parentElement.querySelector(".control-view");
+  const sectionContent = parentElement.querySelectorAll(".section-content");
+  if (viewMore.classList.value.includes("view-more")) {
+    sectionContent.forEach((element) => {
+      element.style.display = "block";
+    });
+    viewMore.classList.remove("view-more");
+    viewMore.classList.add("view-less");
+    viewMore.innerHTML = "View less";
+  } else {
+    sectionContent.forEach((element) => {
+      element.style.display = "none";
+    });
+    viewMore.classList.remove("view-less");
+    viewMore.classList.add("view-more");
+    viewMore.innerHTML = "View more";
+  }
+}
